01eecc73660a4dd72ad9d0dfee5df9eeb41481fc
leanbenac <benacleandro@gmail.com>
2022-11-18 16:59:10 -0300

    new app Gifexpert

---
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+    return (
+        <>
+            {/* {titulo} */}
+            <h1>GifExpertApp</h1>
+
+            {/* {input} */}
+
+            {/* {Listado gif} */}
+            {/* {gitItem} */}
+        </>
+    )
+}
+
+export default GifExpertApp
